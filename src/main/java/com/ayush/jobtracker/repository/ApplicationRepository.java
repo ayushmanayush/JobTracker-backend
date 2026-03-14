ee7c240511b28525358d557b61e90ca13bbfff3f
ayush <ishusingh1404@gmail.com>
2026-03-15 00:18:01 +0530

    testing

--- src/main/java/com/ayush/jobtracker/repository/ApplicationRepository.java
+++ src/main/java/com/ayush/jobtracker/repository/ApplicationRepository.java
@@ -12,5 +12,6 @@ import com.ayush.jobtracker.entity.*;
 public interface ApplicationRepository extends JpaRepository<Application,Long>{
     long countByUserId(Long userId);
     long countByUserIdAndStatus(Long userId, ApplicationStatus status);
+    long countByUserIdAndStatusIn(Long userId, List<ApplicationStatus> statuses);
     List<Application> findByUserId(Long userId);
 }
